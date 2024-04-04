7c2814cbfac8df5c63a819d5c63d1c650f257558
yumin <yumin.dima@gmail.com>
2024-04-04 21:18:11 +0300

    Calculator

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
+
+int Sum(int a, int b);
+int Dif(int a, int b);
+int Prod(int a, int b);
+double Quot(int a, int b); 
+
 void main() {
 	setlocale(LC_ALL, "");
 	cout << "Hello Functions" << endl;
+	int a, b;
+	cout << "¬ведите два числа: "; cin >> a >> b;
+	cout << a << " + " << b << " = " << Sum(a,b) << endl;
+	cout << a << " - " << b << " = " << Dif(a,b) << endl;
+	cout << a << " * " << b << " = " << Prod(a, b) << endl;
+	cout << a << " / " << b << " = " << Quot(a, b) << endl;
+}
+
+int Sum(int a, int b) {
+	int c = a + b;
+	return c;
+}
+int Dif(int a, int b) {
+	return a - b;
+}
+int Prod(int a, int b) {
+	return a * b;
+}
+double Quot(int a, int b) {
+	return (double) a / b;
 }
