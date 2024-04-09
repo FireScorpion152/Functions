ded56a82bbd15b23117cf9a17944f1d7942aa11f
yumin <yumin.dima@gmail.com>
2024-04-09 19:00:45 +0300

    HomeWork

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -11,14 +11,16 @@ int Pow(int a, int b);
 void main() {
 	setlocale(LC_ALL, "");
 	cout << "Hello Functions" << endl;
-	int a, b;
+	int a, b, c, d, h;
 	cout << "¬ведите два числа: "; cin >> a >> b;
 	cout << a << " + " << b << " = " << Sum(a,b) << endl;
 	cout << a << " - " << b << " = " << Dif(a,b) << endl;
 	cout << a << " * " << b << " = " << Prod(a, b) << endl;
 	cout << a << " / " << b << " = " << Quot(a, b) << endl;
-	cout << a << " ^ " << b << " = " << Pow(a, b) << endl;
-	cout << a << "!" << "="  <<  Factorial(a) << endl;
+	cout << "¬ведите число: "; cin >> c; cout << "¬ведите основание степени: "; cin >> d;
+	cout << a << " ^ " << b << " = " << Pow(c, d) << endl;
+	cout << "¬ведите число дл€ факториала: "; cin >> h;
+	cout << a << "!" << "="  <<  Factorial(h) << endl;
 }
 
 int Sum(int a, int b) {
